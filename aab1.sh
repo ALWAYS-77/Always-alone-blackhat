a5cecab032a1c5b0a8e9d45fbb1d25ba76d3274b
ALWAYS-77 <99393231+ALWAYS-77@users.noreply.github.com>
2022-02-10 16:07:00 +0700

    Add files via upload

---
+++ aab1.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo $me"Konfirmasi bahwa anda tidak akan merecode sc ini"
+#jangan di rekode ya ngab:(
+sleep 3
+clear
+echo "LOGIN"
+read -p "Username :" nama
+read -p "password :" pass
+
+echo "Tunggu sebentar lagi proses..."
+echo "#######[User]#######"
+echo "  username : $nama"
+echo "  password : $pass"
+echo "####################"
+sleep 3
+echo $ku"Sukses masuk selamat menggunakan tools"
+sh aab.sh
